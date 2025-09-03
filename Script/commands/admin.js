64e359bc052b41c8b46b8be83113541ea5fd1452
rjraselmahmud <hadaihudai4@gmail.com>
2025-09-04 02:37:08 +0600

    Update admin.js

--- Script/commands/admin.js
+++ Script/commands/admin.js
@@ -1,46 +1,62 @@
-const axios = require("axios");
-const request = require("request");
-const fs = require("fs-extra");
-const moment = require("moment-timezone");
-
 module.exports.config = {
-    name: "admin",
-    version: "1.0.0",
-    hasPermssion: 0,
-    credits: "ULLASH", //don't change my credit 
-    description: "Show Owner Info",
-    commandCategory: "info",
-    usages: "",
-    cooldowns: 5
+  name: "owner",
+  version: "1.0.0",
+  hasPermssion: 0,
+  credits: "Rasel Mahmud",
+  description: "Show bot admin info",
+  commandCategory: "system",
+  usages: "[botadmin/owner/bot admin]",
+  cooldowns: 5
 };
 
-module.exports.run = async function({ api, event }) {
-    var time = moment().tz("Asia/Dhaka").format("DD/MM/YYYY hh:mm:ss A");
+module.exports.run = async function ({ api, event }) {
+  const botOwnerName = "Rasel Mahmud";
+  const botOwnerHeight = "5 feet 8 inches";
+  const botOwner = "Lives in Mymensingh";
+  const botOwnerLocation = "studies in Rajshahi";
+  const facebook1 = "https://www.facebook.com/raselmahmud.q";
+  const facebook2 = "https://www.facebook.com/iiii.482394";
+  const youtube = "https://youtube.com/@rmsilentgaming";
 
-    var callback = () => api.sendMessage({
-        body: `
-┏━━━━━━━━━━━━━━━━━━━━━┓
-┃      🌟 𝗢𝗪𝗡𝗘𝗥 𝗜𝗡𝗙𝗢 🌟      
-┣━━━━━━━━━━━━━━━━━━━━━┫
-┃ 👤 𝐍𝐚𝐦𝐞      : 𝐮 𝐥 𝐥 𝐚 𝐬 𝐡 ッ
-┃ 🚹 𝐆𝐞𝐧𝐝𝐞𝐫    : 𝐌𝐚𝐥𝐞
-┃ ❤️ 𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧  : 𝐈𝐧 𝐂𝐨𝐦𝐩𝐥𝐢𝐜𝐚𝐭𝐞𝐝
-┃ 🎂 𝐀𝐠𝐞       : 21
-┃ 🕌 𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧  : 𝐈𝐬𝐥𝐚𝐦
-┃ 🏫 𝐄𝐝𝐮𝐜𝐚𝐭𝐢𝐨𝐧 : 𝐝𝐢𝐩𝐥𝐨𝐦𝐚 𝐢𝐧 𝐀𝐠𝐫𝐢𝐜𝐮𝐥𝐭𝐮𝐫𝐞
-┃ 🏡 𝐀𝐝𝐝𝐫𝐞𝐬𝐬  : 𝐍𝐨𝐚𝐤𝐡𝐚𝐥𝐢, 𝐁𝐚𝐧𝐠𝐥𝐚𝐝𝐞𝐬𝐡
-┣━━━━━━━━━━━━━━━━━━━━━┫
-┃ 🎭 𝐓𝐢𝐤𝐭𝐨𝐤  : ullash01
-┃ 📢 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 : https://t.me/The_morning_star71
-┃ 🌐 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : https://www.facebook.com/profile.php?id=100015168369582
-┣━━━━━━━━━━━━━━━━━━━━━┫
-┃ 🕒 𝐔𝐩𝐝𝐚𝐭𝐞𝐝 𝐓𝐢𝐦𝐞:  ${time}
-┗━━━━━━━━━━━━━━━━━━━━━┛
-        `,
-        attachment: fs.createReadStream(__dirname + "/cache/1.png")
-    }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.png"));
-  
-    return request(encodeURI(`https://graph.facebook.com/100000478146113/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`))
-        .pipe(fs.createWriteStream(__dirname + '/cache/1.png'))
-        .on('close', () => callback());
+  const msg = `
+╔═══════◇🌟◇═══════╗
+         𝘽𝙊𝙏 𝙊𝙒𝙉𝙀𝙍 𝙄𝙉𝙁𝙊
+╚═══════◇💠◇═══════╝
+
+╔🪪 Name      		: ${botOwnerName}					
+╠📏 Height     		: ${botOwnerHeight}
+╠🏠 Location   		: ${botOwner}
+╠🎓 Education  : ${botOwnerLocation}
+╠🔗 Facebook 1  	: ${facebook1}
+╠🔗 Facebook 2  	: ${facebook2}
+╠🛡️ YouTube    	: ${youtube}
+╚═══════════════════╝
+
+═══════════◇✨◇═════════
+Thanks for your interest in the owner!
+   									𝐇𝐞𝐈𝐢•𝗟𝗨𝗠𝗢 💎✨
+═══════════◇🔮◇═════════
+`;
+
+  const imgURL = "https://graph.facebook.com/100024220812646/picture?height=720&width=720&access_token=6628568379|c1e620fa708a1d5696fb991c1bde5662";
+  const axios = require("axios");
+  const fs = require("fs-extra");
+  const path = __dirname + "/admin_image.jpg";
+
+  try {
+    const res = await axios.get(imgURL, { responseType: "arraybuffer" });
+    fs.writeFileSync(path, Buffer.from(res.data, "binary"));
+
+    api.sendMessage({
+      body: msg,
+      attachment: fs.createReadStream(path)
+    }, event.threadID, () => {
+      fs.unlinkSync(path);
+    }, event.messageID);
+
+    api.setMessageReaction("🤺", event.messageID, () => {}, true);
+  } catch (e) {
+    console.error("Error fetching image:", e);
+    api.sendMessage(msg, event.threadID, event.messageID);
+  }
 };
