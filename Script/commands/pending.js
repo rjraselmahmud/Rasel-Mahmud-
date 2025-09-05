de41183ccee9ae545ae91064abe828dc2b184cd6
rjraselmahmud <hadaihudai4@gmail.com>
2025-09-06 00:12:48 +0600

    Update pending.js

--- Script/commands/pending.js
+++ Script/commands/pending.js
@@ -1,7 +1,7 @@
 module.exports.config = {
 	name: "pending",
 	version: "1.0.5",
-	credits: "MAHBUB SHAON",
+	credits: "Rasel Mahmud",
 	hasPermssion: 2,
 	description: "Manage bot's waiting messages",
 	commandCategory: "system",
@@ -22,7 +22,7 @@ module.exports.languages = {
  "en": {
  "invaildNumber": "%1 is not an invalid number",
  "cancelSuccess": "Refused %1 thread!",
- "notiBox": "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 × • 𝗜𝘀𝗹𝗮𝗺𝗶𝗰𝗸 𝗰𝗵𝗮𝘁 𝗯𝗼𝘁 | ᵁᴸᴸ⁴ˢᴴ Connected Successfully!\nUse /help for more info :>",
+ "notiBox": "𝐇𝐞𝐈𝐢•𝗟𝗨𝗠𝗢 💎✨𝐇𝓾𝐛 • Connected Successfully!\nUse /help for more info :>",
  "approveSuccess": "Approved successfully %1 threads!",
 
  "cantGetPendingList": "Can't get the pending list!",
